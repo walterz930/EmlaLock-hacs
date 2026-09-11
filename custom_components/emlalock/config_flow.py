997481ef7cc4d75107c0087e76c0e3d367371e24
walterz930 <120287113+walterz930@users.noreply.github.com>
2026-09-11 13:23:54 +0100

    Clean up EmlaLock config flow

--- custom_components/emlalock/config_flow.py
+++ custom_components/emlalock/config_flow.py
@@ -5,7 +5,15 @@ from homeassistant import config_entries
 from homeassistant.const import CONF_API_KEY
 
 from .api import EmlaLockApi, EmlaLockApiError
-from .const import CONF_ROLE, CONF_USER_ID, CONF_WEARER_API_KEY, CONF_WEARER_USER_ID, DOMAIN, ROLE_HOLDER, ROLE_WEARER
+from .const import (
+    CONF_ROLE,
+    CONF_USER_ID,
+    CONF_WEARER_API_KEY,
+    CONF_WEARER_USER_ID,
+    DOMAIN,
+    ROLE_HOLDER,
+    ROLE_WEARER,
+)
 
 
 class EmlaLockConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -13,33 +21,81 @@ class EmlaLockConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         errors = {}
-        if user_input:
+
+        if user_input is not None:
             try:
-                data = await EmlaLockApi(self.hass, user_input[CONF_USER_ID], user_input[CONF_API_KEY]).info()
-                if user_input[CONF_ROLE] == ROLE_HOLDER:
-                    if not user_input.get(CONF_WEARER_USER_ID) or not user_input.get(CONF_WEARER_API_KEY):
-                        errors["base"] = "wearer_credentials_required"
-                    else:
-                        await EmlaLockApi(self.hass, user_input[CONF_WEARER_USER_ID], user_input[CONF_WEARER_API_KEY]).info()
-                if errors:
-                    raise EmlaLockApiError(errors["base"])
-            except EmlaLockApiError as err:
-                errors.setdefault("base", "wearer_credentials_required" if str(err) == "wearer_credentials_required" else "invalid_auth")
+                info = await EmlaLockApi(
+                    self.hass,
+                    user_input[CONF_USER_ID],
+                    user_input[CONF_API_KEY],
+                ).info()
+            except EmlaLockApiError:
+                errors["base"] = "invalid_auth"
             else:
-                user = data.get("user", {})
-                username = user.get("username", user_input[CONF_USER_ID])
-                await self.async_set_unique_id(f"{user.get('userid', user_input[CONF_USER_ID])}-{user_input[CONF_ROLE]}")
-                self._abort_if_unique_id_configured()
-                return self.async_create_entry(title=f"EmlaLock - {username} ({user_input[CONF_ROLE]})", data=user_input)
+                self._user_input = user_input
+                if user_input[CONF_ROLE] == ROLE_HOLDER:
+                    return await self.async_step_holder()
+
+                return await self._create_entry(info)
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({
-                vol.Required(CONF_USER_ID): str,
-                vol.Required(CONF_API_KEY): str,
-                vol.Required(CONF_ROLE, default=ROLE_WEARER): vol.In([ROLE_WEARER, ROLE_HOLDER]),
-                vol.Optional(CONF_WEARER_USER_ID): str,
-                vol.Optional(CONF_WEARER_API_KEY): str,
-            }),
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_USER_ID): str,
+                    vol.Required(CONF_API_KEY): str,
+                    vol.Required(CONF_ROLE, default=ROLE_WEARER): vol.In(
+                        [ROLE_WEARER, ROLE_HOLDER]
+                    ),
+                }
+            ),
             errors=errors,
+        )
+
+    async def async_step_holder(self, user_input=None):
+        errors = {}
+
+        if user_input is not None:
+            try:
+                await EmlaLockApi(
+                    self.hass,
+                    user_input[CONF_WEARER_USER_ID],
+                    user_input[CONF_WEARER_API_KEY],
+                ).info()
+            except EmlaLockApiError:
+                errors["base"] = "invalid_wearer_auth"
+            else:
+                self._user_input.update(user_input)
+                try:
+                    info = await EmlaLockApi(
+                        self.hass,
+                        self._user_input[CONF_USER_ID],
+                        self._user_input[CONF_API_KEY],
+                    ).info()
+                except EmlaLockApiError:
+                    errors["base"] = "invalid_auth"
+                else:
+                    return await self._create_entry(info)
+
+        return self.async_show_form(
+            step_id="holder",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_WEARER_USER_ID): str,
+                    vol.Required(CONF_WEARER_API_KEY): str,
+                }
+            ),
+            errors=errors,
+        )
+
+    async def _create_entry(self, info):
+        user = info.get("user", {})
+        username = user.get("username", self._user_input[CONF_USER_ID])
+        await self.async_set_unique_id(
+            f"{user.get('userid', self._user_input[CONF_USER_ID])}-{self._user_input[CONF_ROLE]}"
+        )
+        self._abort_if_unique_id_configured()
+        return self.async_create_entry(
+            title=f"EmlaLock - {username} ({self._user_input[CONF_ROLE]})",
+            data=self._user_input,
         )
